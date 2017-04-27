954f936e97a3255fb4e5c934b70c8ca6e866c980
Lincoln de Sousa <lincoln@comum.org>
2017-04-27 10:05:19 -0400

    libofx: Use macro to declare local strings

--- ofx.cc
+++ ofx.cc
@@ -3,6 +3,9 @@
 #include <nan.h>
 #include <libofx/libofx.h>
 
+#define NEW_STR_LOCAL(x) Nan::New(x).ToLocalChecked()
+
+
 int statementCallback(const struct OfxStatementData ofxStatement, void *data)
 {
   v8::Isolate* isolate = v8::Isolate::GetCurrent();
@@ -11,49 +14,41 @@ int statementCallback(const struct OfxStatementData ofxStatement, void *data)
   struct OfxAccountData ofxAccount = *(ofxStatement.account_ptr);
 
   if (ofxAccount.account_id_valid) {
-    node->Set(Nan::New("id").ToLocalChecked(),
-              Nan::New(ofxAccount.account_id).ToLocalChecked());
-    node->Set(Nan::New("name").ToLocalChecked(),
-              Nan::New(ofxAccount.account_name).ToLocalChecked());
+    node->Set(NEW_STR_LOCAL("id"), NEW_STR_LOCAL(ofxAccount.account_id));
+    node->Set(NEW_STR_LOCAL("name"), NEW_STR_LOCAL(ofxAccount.account_name));
   }
 
   if (ofxAccount.account_number_valid) {
-    node->Set(Nan::New("number").ToLocalChecked(),
-              Nan::New(ofxAccount.account_number).ToLocalChecked());
+    node->Set(NEW_STR_LOCAL("number"), NEW_STR_LOCAL(ofxAccount.account_number));
   }
 
   if (ofxAccount.currency_valid) {
-    node->Set(Nan::New("currency").ToLocalChecked(),
-              Nan::New(ofxAccount.currency).ToLocalChecked());
+    node->Set(NEW_STR_LOCAL("currency"), NEW_STR_LOCAL(ofxAccount.currency));
   }
 
   if (ofxAccount.account_type_valid) {
-    node->Set(Nan::New("type").ToLocalChecked(),
-              Nan::New(ofxAccount.account_type));
+    node->Set(NEW_STR_LOCAL("type"), Nan::New(ofxAccount.account_type));
   }
 
   if (ofxAccount.bank_id_valid) {
-    node->Set(Nan::New("bankId").ToLocalChecked(),
-              Nan::New(ofxAccount.bank_id).ToLocalChecked());
+    node->Set(NEW_STR_LOCAL("bankId"), NEW_STR_LOCAL(ofxAccount.bank_id));
   }
 
   if (ofxAccount.branch_id_valid) {
-    node->Set(Nan::New("branchId").ToLocalChecked(),
-              Nan::New(ofxAccount.branch_id).ToLocalChecked());
+    node->Set(NEW_STR_LOCAL("branchId"), NEW_STR_LOCAL(ofxAccount.branch_id));
   }
 
   // Statement specific fields
   if (ofxStatement.ledger_balance_valid) {
-    node->Set(Nan::New("balance").ToLocalChecked(),
-              Nan::New(ofxStatement.ledger_balance));
+    node->Set(NEW_STR_LOCAL("balance"), Nan::New(ofxStatement.ledger_balance));
   }
   if (ofxStatement.ledger_balance_date_valid) {
-    node->Set(Nan::New("balanceDate").ToLocalChecked(),
+    node->Set(NEW_STR_LOCAL("balanceDate"),
               v8::Date::New(isolate, ofxStatement.ledger_balance_date * 1000));
   }
 
   v8::Local<v8::Object> root = *((v8::Local<v8::Object> *) data);
-  root->Set(Nan::New("account").ToLocalChecked(), node);
+  root->Set(NEW_STR_LOCAL("account"), node);
   return 0;
 }
 
@@ -63,27 +58,27 @@ int transactionCallback(const struct OfxTransactionData data, void *cbData)
   v8::Local<v8::Object> node = v8::Object::New(isolate);
 
   if (data.amount_valid) {
-    node->Set(Nan::New("amount").ToLocalChecked(), Nan::New(data.amount));
+    node->Set(NEW_STR_LOCAL("amount"), Nan::New(data.amount));
   }
 
   if (data.name_valid) {
-    node->Set(Nan::New("name").ToLocalChecked(), Nan::New(data.name).ToLocalChecked());
+    node->Set(NEW_STR_LOCAL("name"), NEW_STR_LOCAL(data.name));
   }
 
   if (data.memo_valid) {
-    node->Set(Nan::New("memo").ToLocalChecked(), Nan::New(data.memo).ToLocalChecked());
+    node->Set(NEW_STR_LOCAL("memo"), NEW_STR_LOCAL(data.memo));
   }
 
   if (data.transactiontype_valid) {
-    node->Set(Nan::New("type").ToLocalChecked(), Nan::New(data.transactiontype));
+    node->Set(NEW_STR_LOCAL("type"), Nan::New(data.transactiontype));
   }
 
   if (data.fi_id_valid) {
-    node->Set(Nan::New("fitId").ToLocalChecked(), Nan::New(data.fi_id).ToLocalChecked());
+    node->Set(NEW_STR_LOCAL("fitId"), NEW_STR_LOCAL(data.fi_id));
   }
 
   if (data.date_posted_valid) {
-    node->Set(Nan::New("datePosted").ToLocalChecked(),
+    node->Set(NEW_STR_LOCAL("datePosted"),
               v8::Date::New(isolate, data.date_posted * 1000));
   }
 
@@ -110,7 +105,7 @@ void parseFile(const Nan::FunctionCallbackInfo<v8::Value>& args)
   v8::Isolate* isolate = args.GetIsolate();
   v8::Local<v8::Object> result = v8::Object::New(isolate);
   v8::Local<v8::Array> transactions = v8::Array::New(isolate);
-  result->Set(Nan::New("transactions").ToLocalChecked(), transactions);
+  result->Set(NEW_STR_LOCAL("transactions"), transactions);
 
   // Parse file
   LibofxContextPtr context = libofx_get_new_context();
@@ -127,41 +122,41 @@ void Constants(v8::Local<v8::Object> exports) {
 
   // OfxAccountData::AccountType
   v8::Local<v8::Object> atNode = v8::Object::New(isolate);
-  atNode->Set(Nan::New("SAVINGS").ToLocalChecked(), Nan::New(OfxAccountData::OFX_SAVINGS));
-  atNode->Set(Nan::New("CHECKING").ToLocalChecked(), Nan::New(OfxAccountData::OFX_CHECKING));
-  atNode->Set(Nan::New("MONEYMRKT").ToLocalChecked(), Nan::New(OfxAccountData::OFX_MONEYMRKT));
-  atNode->Set(Nan::New("CREDITLINE").ToLocalChecked(), Nan::New(OfxAccountData::OFX_CREDITLINE));
-  atNode->Set(Nan::New("CMA").ToLocalChecked(), Nan::New(OfxAccountData::OFX_CMA));
-  atNode->Set(Nan::New("CREDITCARD").ToLocalChecked(), Nan::New(OfxAccountData::OFX_CREDITCARD));
-  atNode->Set(Nan::New("INVESTMENT").ToLocalChecked(), Nan::New(OfxAccountData::OFX_INVESTMENT));
-  exports->Set(Nan::New("AccountType").ToLocalChecked(), atNode);
+  atNode->Set(NEW_STR_LOCAL("CMA"), Nan::New(OfxAccountData::OFX_CMA));
+  atNode->Set(NEW_STR_LOCAL("SAVINGS"), Nan::New(OfxAccountData::OFX_SAVINGS));
+  atNode->Set(NEW_STR_LOCAL("CHECKING"), Nan::New(OfxAccountData::OFX_CHECKING));
+  atNode->Set(NEW_STR_LOCAL("MONEYMRKT"), Nan::New(OfxAccountData::OFX_MONEYMRKT));
+  atNode->Set(NEW_STR_LOCAL("CREDITLINE"), Nan::New(OfxAccountData::OFX_CREDITLINE));
+  atNode->Set(NEW_STR_LOCAL("CREDITCARD"), Nan::New(OfxAccountData::OFX_CREDITCARD));
+  atNode->Set(NEW_STR_LOCAL("INVESTMENT"), Nan::New(OfxAccountData::OFX_INVESTMENT));
+  exports->Set(NEW_STR_LOCAL("AccountType"), atNode);
 
   // TransactionType
   v8::Local<v8::Object> ttNode = v8::Object::New(isolate);
-  ttNode->Set(Nan::New("CREDIT").ToLocalChecked(), Nan::New(OFX_CREDIT));
-  ttNode->Set(Nan::New("DEBIT").ToLocalChecked(), Nan::New(OFX_DEBIT));
-  ttNode->Set(Nan::New("INT").ToLocalChecked(), Nan::New(OFX_INT));
-  ttNode->Set(Nan::New("DIV").ToLocalChecked(), Nan::New(OFX_DIV));
-  ttNode->Set(Nan::New("FEE").ToLocalChecked(), Nan::New(OFX_FEE));
-  ttNode->Set(Nan::New("SRVCHG").ToLocalChecked(), Nan::New(OFX_SRVCHG));
-  ttNode->Set(Nan::New("DEP").ToLocalChecked(), Nan::New(OFX_DEP));
-  ttNode->Set(Nan::New("ATM").ToLocalChecked(), Nan::New(OFX_ATM));
-  ttNode->Set(Nan::New("POS").ToLocalChecked(), Nan::New(OFX_POS));
-  ttNode->Set(Nan::New("XFER").ToLocalChecked(), Nan::New(OFX_XFER));
-  ttNode->Set(Nan::New("CHECK").ToLocalChecked(), Nan::New(OFX_CHECK));
-  ttNode->Set(Nan::New("PAYMENT").ToLocalChecked(), Nan::New(OFX_PAYMENT));
-  ttNode->Set(Nan::New("CASH").ToLocalChecked(), Nan::New(OFX_CASH));
-  ttNode->Set(Nan::New("DIRECTDEP").ToLocalChecked(), Nan::New(OFX_DIRECTDEP));
-  ttNode->Set(Nan::New("DIRECTDEBIT").ToLocalChecked(), Nan::New(OFX_DIRECTDEBIT));
-  ttNode->Set(Nan::New("REPEATPMT").ToLocalChecked(), Nan::New(OFX_REPEATPMT));
-  ttNode->Set(Nan::New("OTHER").ToLocalChecked(), Nan::New(OFX_OTHER));
-  exports->Set(Nan::New("TransactionType").ToLocalChecked(), ttNode);
+  ttNode->Set(NEW_STR_LOCAL("CREDIT"), Nan::New(OFX_CREDIT));
+  ttNode->Set(NEW_STR_LOCAL("DEBIT"), Nan::New(OFX_DEBIT));
+  ttNode->Set(NEW_STR_LOCAL("INT"), Nan::New(OFX_INT));
+  ttNode->Set(NEW_STR_LOCAL("DIV"), Nan::New(OFX_DIV));
+  ttNode->Set(NEW_STR_LOCAL("FEE"), Nan::New(OFX_FEE));
+  ttNode->Set(NEW_STR_LOCAL("SRVCHG"), Nan::New(OFX_SRVCHG));
+  ttNode->Set(NEW_STR_LOCAL("DEP"), Nan::New(OFX_DEP));
+  ttNode->Set(NEW_STR_LOCAL("ATM"), Nan::New(OFX_ATM));
+  ttNode->Set(NEW_STR_LOCAL("POS"), Nan::New(OFX_POS));
+  ttNode->Set(NEW_STR_LOCAL("XFER"), Nan::New(OFX_XFER));
+  ttNode->Set(NEW_STR_LOCAL("CHECK"), Nan::New(OFX_CHECK));
+  ttNode->Set(NEW_STR_LOCAL("PAYMENT"), Nan::New(OFX_PAYMENT));
+  ttNode->Set(NEW_STR_LOCAL("CASH"), Nan::New(OFX_CASH));
+  ttNode->Set(NEW_STR_LOCAL("DIRECTDEP"), Nan::New(OFX_DIRECTDEP));
+  ttNode->Set(NEW_STR_LOCAL("DIRECTDEBIT"), Nan::New(OFX_DIRECTDEBIT));
+  ttNode->Set(NEW_STR_LOCAL("REPEATPMT"), Nan::New(OFX_REPEATPMT));
+  ttNode->Set(NEW_STR_LOCAL("OTHER"), Nan::New(OFX_OTHER));
+  exports->Set(NEW_STR_LOCAL("TransactionType"), ttNode);
 }
 
 void Init(v8::Local<v8::Object> exports)
 {
   Constants(exports);
-  exports->Set(Nan::New("parseFile").ToLocalChecked(),
+  exports->Set(NEW_STR_LOCAL("parseFile"),
                Nan::New<v8::FunctionTemplate>(parseFile)->GetFunction());
 }
 
