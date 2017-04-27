6bb7467459b22ff2901e53fe70e218fa463638ca
Lincoln de Sousa <lincoln@comum.org>
2017-04-27 02:06:59 -0400

    libofx: Parse transactions

--- ofx.cc
+++ ofx.cc
@@ -57,6 +57,43 @@ int statementCallback(const struct OfxStatementData ofxStatement, void *data)
   return 0;
 }
 
+int transactionCallback(const struct OfxTransactionData data, void *cbData)
+{
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::Local<v8::Object> node = v8::Object::New(isolate);
+
+  if (data.amount_valid) {
+    node->Set(Nan::New("amount").ToLocalChecked(), Nan::New(data.amount));
+  }
+
+  if (data.name_valid) {
+    node->Set(Nan::New("name").ToLocalChecked(), Nan::New(data.name).ToLocalChecked());
+  }
+
+  if (data.memo_valid) {
+    node->Set(Nan::New("memo").ToLocalChecked(), Nan::New(data.memo).ToLocalChecked());
+  }
+
+  if (data.transactiontype_valid) {
+    node->Set(Nan::New("type").ToLocalChecked(), Nan::New(data.transactiontype));
+  }
+
+  if (data.fi_id_valid) {
+    node->Set(Nan::New("fitid").ToLocalChecked(), Nan::New(data.fi_id).ToLocalChecked());
+  }
+
+  if (data.date_posted_valid) {
+    node->Set(Nan::New("datePosted").ToLocalChecked(),
+              v8::Date::New(isolate, data.date_posted * 1000));
+  }
+
+  // Append the above node to the transactions array
+  v8::Local<v8::Array> arrTransactions =
+    v8::Local<v8::Array>::Cast(*((v8::Local<v8::Array> *) cbData));
+  arrTransactions->Set(arrTransactions->Length(), node);
+  return 0;
+}
+
 void parseFile(const Nan::FunctionCallbackInfo<v8::Value>& args)
 {
   Nan::Utf8String filePathArg(args[0]);
@@ -72,10 +109,13 @@ void parseFile(const Nan::FunctionCallbackInfo<v8::Value>& args)
   // Create result object
   v8::Isolate* isolate = args.GetIsolate();
   v8::Local<v8::Object> result = v8::Object::New(isolate);
+  v8::Local<v8::Array> transactions = v8::Array::New(isolate);
+  result->Set(Nan::New("transactions").ToLocalChecked(), transactions);
 
   // Parse file
   LibofxContextPtr context = libofx_get_new_context();
   ofx_set_statement_cb(context, statementCallback, &result);
+  ofx_set_transaction_cb(context, transactionCallback, &transactions);
   libofx_proc_file(context, filePath, OFX);
 
   args.GetReturnValue().Set(result);
@@ -94,6 +134,27 @@ void Constants(v8::Local<v8::Object> exports) {
   atNode->Set(Nan::New("CREDITCARD").ToLocalChecked(), Nan::New(OfxAccountData::OFX_CREDITCARD));
   atNode->Set(Nan::New("INVESTMENT").ToLocalChecked(), Nan::New(OfxAccountData::OFX_INVESTMENT));
   exports->Set(Nan::New("AccountType").ToLocalChecked(), atNode);
+
+  // TransactionType
+  v8::Local<v8::Object> ttNode = v8::Object::New(isolate);
+  ttNode->Set(Nan::New("CREDIT").ToLocalChecked(), Nan::New(OFX_CREDIT));
+  ttNode->Set(Nan::New("DEBIT").ToLocalChecked(), Nan::New(OFX_DEBIT));
+  ttNode->Set(Nan::New("INT").ToLocalChecked(), Nan::New(OFX_INT));
+  ttNode->Set(Nan::New("DIV").ToLocalChecked(), Nan::New(OFX_DIV));
+  ttNode->Set(Nan::New("FEE").ToLocalChecked(), Nan::New(OFX_FEE));
+  ttNode->Set(Nan::New("SRVCHG").ToLocalChecked(), Nan::New(OFX_SRVCHG));
+  ttNode->Set(Nan::New("DEP").ToLocalChecked(), Nan::New(OFX_DEP));
+  ttNode->Set(Nan::New("ATM").ToLocalChecked(), Nan::New(OFX_ATM));
+  ttNode->Set(Nan::New("POS").ToLocalChecked(), Nan::New(OFX_POS));
+  ttNode->Set(Nan::New("XFER").ToLocalChecked(), Nan::New(OFX_XFER));
+  ttNode->Set(Nan::New("CHECK").ToLocalChecked(), Nan::New(OFX_CHECK));
+  ttNode->Set(Nan::New("PAYMENT").ToLocalChecked(), Nan::New(OFX_PAYMENT));
+  ttNode->Set(Nan::New("CASH").ToLocalChecked(), Nan::New(OFX_CASH));
+  ttNode->Set(Nan::New("DIRECTDEP").ToLocalChecked(), Nan::New(OFX_DIRECTDEP));
+  ttNode->Set(Nan::New("DIRECTDEBIT").ToLocalChecked(), Nan::New(OFX_DIRECTDEBIT));
+  ttNode->Set(Nan::New("REPEATPMT").ToLocalChecked(), Nan::New(OFX_REPEATPMT));
+  ttNode->Set(Nan::New("OTHER").ToLocalChecked(), Nan::New(OFX_OTHER));
+  exports->Set(Nan::New("TransactionType").ToLocalChecked(), ttNode);
 }
 
 void Init(v8::Local<v8::Object> exports)
