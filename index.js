eea3db20752f0c9a052f03f8a52ce2f60bd38a3f
Lincoln de Sousa <lincoln@comum.org>
2017-05-09 12:16:14 -0400

    Remove unused code in libofx

--- index.js
+++ index.js
@@ -3,7 +3,4 @@ import bindings from 'bindings';
 
 const ofx = bindings({ bindings: 'ofx', module_root: __dirname });
 
-ofx.transactionTypeName = (type) =>
-  ofx.TransactionTypeNames[type].toLowerCase();
-
 export default ofx;
